c2ba7bdd13cc68f43af9bbfd96c70b0c6e4bfb14
edwinclever89 <34646008+edwinclever89@users.noreply.github.com>
2023-07-04 22:00:19 -0700

    Add files via upload

---
+++ project/bookingform.php
@@ -0,0 +1,359 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+ 
+ 
+
+<!-- Title  Section -->
+<div class="row" style="padding-top:50px">
+  <div class="col-lg-2"></div>
+  <div class="col-lg-8">
+<div class="alert alert-secondary" role="alert">
+ <center><div class="h4"> Death Announcement Booking Form</div></center>
+</div></div>
+<div class="col-lg-2"></div>
+
+
+</div>
+
+<!-- End of Title Section -->
+
+<!-- Second title Section -->
+
+<div class="row">
+
+<div class="col-lg-2"></div>
+  <div class="col-lg-8">
+ 
+ <center> <div class="h3"> Tangazo la Kifo</div> </center>
+</div></div>
+
+</div>
+
+
+<!-- Name Section -->
+  <div class="row" >
+    <div class="col-md-8 col-md-offset-2">
+      <form class="form-horizontal" role="form">
+        <fieldset>
+
+          <!-- Form Name -->
+          <legend>Personal Information Details</legend>
+
+          <!-- Text input-->
+       
+          <div class="form-group">
+
+          <div class="col-sm-2">
+          <select class="form-select" id="inputGroupSelect01">
+          <option selected>Choose Status</option>
+          <option value="1">Prof</option>
+          <option value="2">Dr</option>
+          <option value="3">Askofu</option>
+          <option value="3">Mchungaji</option>
+          <option value="3">Sheikh</option>
+          <option value="3">Ustadhi</option>
+          </select>
+            </div>
+
+
+
+            <div class="col-sm-4">
+              <input type="text" name="fistName" placeholder="First Name" class="form-control">
+            </div>
+            <div class="col-sm-4">
+              <input type="text" name="middleName" placeholder="Last Name" class="form-control">
+            </div>
+            
+          </div>
+
+           <!-- Text input-->
+           <div class="form-group">
+            <div class="col-sm-4">
+              <input type="text" placeholder="Uhusiano na Marehemu" class="form-control">
+            </div>
+       
+            <div class="col-sm-6">
+              <input type="text" placeholder="Jina Kamili la Marehemu" class="form-control">
+            </div>
+          </div>
+
+
+
+
+          <div class="form-group">
+          
+          <div class="col-sm-4">
+          <label for="exampleFormControlTextarea1" class="form-label">Tarehe ya kifo</label>
+            <input type="date" placeholder="Tarehe ya Kifo" class="form-control">
+           </div>
+
+
+           
+          <div class="col-sm-6">
+            <label for="exampleFormControlTextarea1" class="form-label">Kilichotokea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+           </div>
+
+
+            
+          </div>
+
+
+
+
+
+          <!-- Text input-->
+          <div class="form-group">
+            <div class="col-sm-4">
+            <label for="exampleFormControlTextarea1" class="form-label">Tarehe ya Mazishi</label>
+              <input type="date" placeholder="Date Of Birth" class="form-control">
+            </div>
+
+            <div class="col-sm-6">
+            <label for="exampleFormControlTextarea1" class="form-label">Toa Maelezo ya Mipango ya mazishi</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+           </div>
+   
+
+
+
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group" >
+         
+            
+            <div class="col-sm-12">
+            <legend>Habari Ziwafikie</legend>
+              
+            </div>
+          </div>
+
+
+    <!-- Text input-->
+          <div class="form-group" >
+         
+         <div class="col-sm-4">
+         <input type="text" placeholder="Jina Kamili" class="form-control">
+         
+         </div>
+       </div>
+
+
+         <!-- Text input-->
+         <div class="form-group" >
+         
+         <div class="col-sm-4">
+         <input type="text" placeholder="Jina Kamili" class="form-control">
+         
+         </div>
+       </div>
+
+
+         <!-- Text input-->
+         <div class="form-group" >
+         
+         <div class="col-sm-4">
+         <input type="text" placeholder="Jina Kamili" class="form-control">
+         
+         </div>
+       </div>
+
+
+      
+
+          
+
+<!-- Address Section -->
+          <!-- Form Name -->
+          <legend>Muda Wa Kutangaza</legend>
+          <!-- Text input-->
+          <div class="form-group">
+
+            <div class="col-sm-3">
+
+            <label>Tarehe</label>
+            <input type="date" placeholder="Tarehe ya Kifo" class="form-control">
+
+            </div>
+
+           <div class="col-sm-4">
+
+           <label>Muda</label>
+            
+           <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+               Saa 4
+            </label>
+              </div>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+              <label class="form-check-label" for="flexCheckChecked">
+                Saa 7
+               </label>
+          </div>
+           
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+              <label class="form-check-label" for="flexCheckChecked">
+                Saa 10
+               </label>
+          </div>
+
+
+
+
+
+           </div>
+
+
+            
+            </div>
+        
+           
+         
+<legend></legend>
+
+          
+
+            <div class="form-group">
+
+<div class="col-sm-3">
+
+<label>Tarehe</label>
+<input type="date" placeholder="Tarehe ya Kifo" class="form-control">
+
+</div>
+
+<div class="col-sm-4">
+
+ 
+
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+<label class="form-check-label" for="flexCheckDefault">
+   Saa 4
+</label>
+  </div>
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Saa 7
+   </label>
+</div>
+
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Saa 10
+   </label>
+</div>
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+<legend></legend>
+ <div class="form-group">
+
+<div class="col-sm-3">
+
+<label>Tarehe</label>
+<input type="date" placeholder="Tarehe ya Kifo" class="form-control">
+
+</div>
+
+<div class="col-sm-4">
+
+ 
+
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+<label class="form-check-label" for="flexCheckDefault">
+   Saa 4
+</label>
+  </div>
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Saa 7
+   </label>
+</div>
+
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Saa 10
+   </label>
+</div>
+
+
+
+
+
+</div>
+
+
+
+</div>
+ 
+
+   <!-- Text input-->
+               
+
+        
+    <!-- Text input-->
+    
+  
+ 
+ 
+            <legend>Payment Method</legend>
+            <div class="form-group">
+                <div class="col-sm-2">
+                  <input type="checkbox" name="VietNgu">   Tigo Pesa
+                </div>
+                <div class="col-sm-2">
+                  <input type="checkbox" name="Math">   Mpesa
+                </div>
+                <div class="col-sm-2">
+                  <input type="checkbox" name="paid"  data-toggle="modal" data-target="#payment">   Bank
+                </div>
+          </div>
+<!-- Command -->
+     
+        </fieldset>
+      </form>
+     <div class="row"> <button type="button" class="btn btn-primary">Save</button></div>
+    </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+
+
+ 
+        <!-- /.modal-content --> 
+  </div>
+    <!-- /.modal-dialog --> 
+</div>
+
+ 
+                       
+                         
+                    </form>
+                </div>  <!-- div class="col-md-8 col-md-offset-1" -->
+            </div>  <!-- div class="row" -->
+        </div><!-- /.modal-content --> 
+    </div><!-- div class="modal-dialog -->
+</div>
